c3abb105834ea78234d5618ab5e5f8417e689a8b
Abanoub Nasry <bonypop9@gmail.com>
2024-11-30 23:42:05 +0200

    deploy on vite

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,8 +5,10 @@ function Footer() {
   const updatedYear = year.getFullYear();
   return (
     <div className="footer ">
-      <img src="../public/photos/Eltabib Lab cover page.png" alt="" />
-      <p className="copy-right">Copyright &copy;{updatedYear}.El-TABEB|LAB.All Rights Reserved </p>
+      <img src="public/photos/Eltabib Lab cover page.png" alt="" />
+      <p className="copy-right">
+        Copyright &copy;{updatedYear}.El-TABEB|LAB.All Rights Reserved{" "}
+      </p>
       <div className="shortcuts">
         <span className="icon-facebook-square ">
           <a href="https://www.facebook.com/profile.php?id=100088403545170&name=xhp_nt_">
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/abanoubamgadnsary/Lab-site",
   plugins: [react(), eslint()],
 });
