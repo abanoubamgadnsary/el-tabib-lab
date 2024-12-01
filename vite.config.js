3b5d59fa9dfa0a7c6cbd19282745a2b7498ddbf1
Abanoub Nasry <bonypop9@gmail.com>
2024-12-01 10:05:48 +0200

    logo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Lab-site/",
+  base: "/el-tabib-lab/",
   plugins: [react(), eslint()],
 });
