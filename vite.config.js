08a59d086fc8d8e60d64fc715c20d769a7859f85
Abanoub Nasry <bonypop9@gmail.com>
2024-12-01 08:45:01 +0200

    initialcommit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/abanoubamgadnsary/Lab-site",
+  base: "/Lab-site/",
   plugins: [react(), eslint()],
 });
